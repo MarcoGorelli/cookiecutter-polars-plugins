8dc69e3fb1bd4d0a3d1143d55c7b897399c67c58
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-01-22 15:10:43 +0000

    more boilerplate

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/__init__.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/__init__.py
@@ -1,5 +1,14 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 import polars as pl
 from polars.utils.udfs import _get_shared_lib_location
+
+from {{ cookiecutter.project_slug }}.utils import parse_into_expr
+
+if TYPE_CHECKING:
+    from polars.type_aliases import IntoExpr
 
 lib = _get_shared_lib_location(__file__)
 
