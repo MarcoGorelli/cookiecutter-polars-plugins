e9444f5f19f27491ed14c6b5f5467fc7c5f593d0
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-03-21 12:54:41 +0000

    dont use deprecated functions anywhere

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/__init__.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from pathlib import Path
 from typing import TYPE_CHECKING
 
 import polars as pl
@@ -10,7 +11,12 @@ from {{ cookiecutter.project_slug }}.utils import parse_into_expr, register_plug
 if TYPE_CHECKING:
     from polars.type_aliases import IntoExpr
 
-lib = _get_shared_lib_location(__file__)
+if parse_version(pl.__version__) < parse_version("0.20.16"):
+    from polars.utils.udfs import _get_shared_lib_location
+
+    lib: str | Path = _get_shared_lib_location(__file__)
+else:
+    lib = Path(__file__).parent
 
 def pig_latinnify(expr: IntoExpr) -> pl.Expr:
     expr = parse_into_expr(expr)
